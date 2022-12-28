7a082757c055c00aef17b8ceb19758972d7fbb19
Dolyvm <114623065+Dolyvm@users.noreply.github.com>
2022-12-28 09:40:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from admin_panel import register_admin_handlers
 # token = os.getenv('BOT_KEY')
 from handlers import register_user_handlers
 
-bot = Bot('5012407051:AAG_TsNKPpzj8D6Vc9XKMKxzwG9fFj1n_24')
+bot = Bot('5626261222:AAHKJo2DzuU5vpMJuBQspVbPJmOge6nJaJs')
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
