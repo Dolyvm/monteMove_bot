7cb4f9527fd29fb85c62872098e584ff07103647
husee-code <huseeads@gmail.com>
2023-01-08 21:49:23 +0300

    Добавил возможность смотреть статистику использования бота.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from admin_panel import register_admin_handlers
-# token = os.getenv('BOT_KEY')
 from handlers import register_user_handlers
 
-bot = Bot('5626261222:AAHKJo2DzuU5vpMJuBQspVbPJmOge6nJaJs')
+# bot = Bot('5626261222:AAHKJo2DzuU5vpMJuBQspVbPJmOge6nJaJs')  # prod
+bot = Bot("5012407051:AAG_TsNKPpzj8D6Vc9XKMKxzwG9fFj1n_24")
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
