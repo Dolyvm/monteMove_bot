eb59dda5e453855a21a249ddd7545ec83ee0fa60
husee-code <87664267+husee-code@users.noreply.github.com>
2023-01-25 22:13:48 +0300

    бот висит сразу на тесте

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from aiogram import Bot
+from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+
 
 from admin_panel import register_admin_handlers
 from handlers import register_user_handlers
