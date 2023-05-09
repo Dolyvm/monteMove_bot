118d37bc7e872d91eef02ed6e2fc94c735368d2b
husee-code <87664267+husee-code@users.noreply.github.com>
2023-05-09 14:08:36 +0300

    Update main.py
    поменял бота на прод + убрал forward_chat_id

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import asyncio
-
 from aiogram import Bot
-from aiogram.utils import executor
-from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 from admin_panel import register_admin_handlers
 from handlers import register_user_handlers
 from statistics.stats_functions import scheduler
 
-# bot = Bot('5626261222:AAHKJo2DzuU5vpMJuBQspVbPJmOge6nJaJs')  # prod
-bot = Bot("5012407051:AAFi9pOFidtHbJeeGakeBMso8UwA0HESIwU")  # test
-forward_chat_id = -1001763589351
+bot = Bot('5626261222:AAHKJo2DzuU5vpMJuBQspVbPJmOge6nJaJs')  # prod
+# bot = Bot("5012407051:AAFi9pOFidtHbJeeGakeBMso8UwA0HESIwU")  # test
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
